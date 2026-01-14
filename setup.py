a3a1386ed0d5baf49c7ad674a4c2ea5e4e35e4ec
prakash mali <prakashmali6556@gmail.com>
2026-01-14 15:01:24 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "disaster_victim_detection"
+AUTHOR_USER_NAME = "prakash-saini69"
+SRC_REPO = "victimDetector"
+AUTHOR_EMAIL = "prakashmali6556@gmail.com" # Update this with your actual email
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for disaster victim detection using YOLOv8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
